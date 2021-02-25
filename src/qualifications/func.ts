f3c42c3c7a0b662b9c3302f304e58a7f531d5056
Alex Dachin <alex-sorin.dachin@klarna.com>
2021-02-25 19:16:30 +0100

    Barebones for qualifications

---
+++ src/qualifications/func.ts
@@ -0,0 +1,8 @@
+import common from '../common'
+
+const func = (input: string): string => {
+  common.inspect(input)
+  return input
+}
+
+export default func
---
+++ src/qualifications/types.ts
@@ -0,0 +1,38 @@
+export type StreetName = string
+
+export type Street = {
+  B: number // intersection id at beginning
+  E: number // intersection id at ending
+  L: number // length
+}
+
+export type Car = {
+  P: number // number of streets that car wants to travel
+  streetNames: StreetName[]
+}
+
+export type Intersection = {
+  in: StreetName[]  // streets going in
+  out: StreetName[] // streets going out
+}
+
+export type Input = {
+  D: number // duration of simulation
+  I: number // number of intersections
+  S: number // number of streets
+  V: number // number of cars
+  F: number // bonus points
+  streets: {
+    [name: string]: Street
+  }
+  cars: Car[]
+  intersections: Intersection[]
+}
+
+export type Output = Array<{
+  intersectionId: number
+  schedule: Array<{
+    streetName: StreetName
+    duration: number
+  }>
+}>
